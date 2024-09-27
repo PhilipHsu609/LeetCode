cf479f5c79c9d5b491bb61e02c340a29e600371a
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2024-09-27 15:08:13 +0800

    Time: 16 ms (93.3%), Space: 16.1 MB (73.37%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,40 +2,62 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> res;
+        vector<vector<int>> ret;
+
+        if(n < 4) {
+            return ret;
+        }
+
         sort(nums.begin(), nums.end());
-        int i = 0, prev = nums[0] - 1;
-        while(i < n) {
-            if(nums[i] != prev) {
-                threeSum(nums, res, i, target - nums[i]);
-                prev = nums[i];
+
+        for(int a = 0; a < n; a++) {
+            
+            if(nums[a] * 4ll > target) {
+                break;
             }
-            i++;
-        }    
-        return res;
-    }
-    
-    void threeSum(const vector<int> &nums, vector<vector<int>> &res, const int i, const int target) {
-        int n = nums.size();
-        int j = n - 1, prev = nums[0] - 1;
-        while(j >= 2) {
-            if(j != i && nums[j] != prev) {
-                int l = i + 1, r = j - 1;
-                while(l < r) {
-                    int sum = nums[j] + nums[l] + nums[r];
+            
+            if(a > 0 && nums[a - 1] == nums[a]) {
+                continue;
+            }
+
+            for(int b = a + 1; b < n - 2; b++) {
+
+                if(nums[a] + nums[b] * 3ll > target) {
+                    break;
+                }
+
+                if(b > a + 1 && nums[b - 1] == nums[b]) {
+                    continue;
+                }
+
+                int c = b + 1, d = n - 1;
+
+                if(nums[a] + nums[b] + nums[c] * 2ll > target) {
+                    break;
+                }
+
+                while(c < d) {
+                    long long sum = (long long)nums[a] + nums[b] + nums[c] + nums[d];
+
                     if(sum == target) {
-                        res.push_back({nums[i], nums[l], nums[r], nums[j]});
-                        do{l++;}while(l < r && nums[l - 1] == nums[l]);
-                        do{r--;}while(l < r && nums[r] == nums[r + 1]); 
-                    } else if(sum < target) {
-                        do{l++;}while(l < r && nums[l - 1] == nums[l]);
+                        ret.push_back({nums[a], nums[b], nums[c], nums[d]});
+                    }
+
+                    if(sum >= target) {
+                        d--;
+                        while(d > 0 && nums[d] == nums[d + 1]) {
+                            d--;
+                        }
                     } else {
-                        do{r--;}while(l < r && nums[r] == nums[r + 1]); 
+                        c++;
+                        while(c < n && nums[c - 1] == nums[c]) {
+                            c++;
+                        }
                     }
                 }
-                prev = nums[j];
             }
-            j--;
         }
+
+        return ret;
     }
 };
