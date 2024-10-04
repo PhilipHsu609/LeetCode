f535d5101df8b34b3b5a37996ac196d81a7e7980
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2024-10-04 09:56:42 +0800

    Time: 54 ms (92.6%), Space: 57.4 MB (11%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int n = skill.size();
-        if(n == 2) {
-            return static_cast<long long>(skill[0]) * skill[1];
-        }
-
         int sum = accumulate(skill.begin(), skill.end(), 0);
+        int target = sum / (n / 2);
 
-        if(sum % 2 == 1) {
-            return -1;
+        unordered_map<int, int> cnt; // skill -> count
+        for(int s : skill) {
+            cnt[s]++;
         }
 
-        int target = sum * 2 / n;
-        sort(skill.begin(), skill.end());
-
-        int l = 0, r = n - 1;
         long long ret = 0;
-        while(l < r) {
-            if(skill[l] + skill[r] != target) {
+        while(!cnt.empty()) {
+            int s = cnt.begin()->first;
+            int c = cnt.begin()->second;
+
+            if(cnt.find(target - s) == cnt.end() || cnt[s] != cnt[target - s]) {
                 return -1;
             }
-            ret += static_cast<long long>(skill[l]) * skill[r];
-            l++, r--;
+
+            if(s == target - s) {
+                if(c % 2 == 1) {
+                    return -1;
+                }
+                c /= 2;
+            }
+
+            ret += static_cast<long long>(s) * (target - s) * c;
+
+            cnt.erase(s);
+            cnt.erase(target - s);
         }
 
         return ret;
