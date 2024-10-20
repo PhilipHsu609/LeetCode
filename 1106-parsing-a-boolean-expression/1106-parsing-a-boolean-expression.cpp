b26c17989f45e69c50a431769f5d9f210eb87e2b
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2024-10-20 15:56:33 +0800

    Time: 15 ms (24.89%), Space: 11.9 MB (31.84%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool parseBoolExpr(string expression) {
-        const string &expr = expression;
-        int n = expr.length();
-        return helper(expr, 0, n - 1);
+        int n = expression.length();
+        return helper(expression, 0, n - 1);
     }
 
     bool helper(const string &expr, int start, int end) {
-        vector<int> val;
-
+        vector<int> vals;
         for(int i = start + 2; i <= end - 1; i++) {
             if(expr[i] == '!' || expr[i] == '&' || expr[i] == '|') {
                 int level = 1;
@@ -25,30 +23,30 @@ public:
                     }
                     j++;
                 }
-                val.push_back(helper(expr, i, j) ? 1 : 0);
-                i = j;    
+                vals.push_back(helper(expr, i, j) ? 1 : 0);
+                i = j;
             } else if(expr[i] == 't' || expr[i] == 'f') {
-                val.push_back(expr[i] == 't' ? 1 : 0);
+                vals.push_back(expr[i] == 't' ? 1 : 0);
             }
         }
 
-        return eval(expr[start], val);
+        return eval(expr[start], vals);
     }
 
-    bool eval(char op, const vector<int> &val) {
+    bool eval(char op, const vector<int> &vals) {
         if(op == '!') {
-            return val[0] == 0;
+            return vals[0] == 0;
         }
         if(op == '&') {
             int ret = 1;
-            for(int v : val) {
+            for(int v : vals) {
                 ret *= v;
             }
             return ret == 1;
         }
         if(op == '|') {
             int ret = 0;
-            for(int v : val) {
+            for(int v : vals) {
                 ret += v;
             }
             return ret != 0;
