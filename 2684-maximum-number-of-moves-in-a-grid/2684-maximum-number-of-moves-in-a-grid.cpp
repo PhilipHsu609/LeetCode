c5130f63a428eafe00c12c549fd09895af7b16c9
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2024-10-29 10:47:37 +0800

    Time: 12 ms (73.91%), Space: 74.1 MB (73.46%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -11,26 +11,17 @@ public:
 
         int ret = 0;
         for(int j = 1; j < n; j++) {
-            bool stop = true;
             for(int i = 0; i < m; i++) {
                 if(i > 0 && dp[i - 1][j - 1] > 0 && grid[i][j] > grid[i - 1][j - 1]) {
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
-                    stop = false;
                 }
                 if(dp[i][j - 1] > 0 && grid[i][j] > grid[i][j - 1]) {
                     dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1);
-                    stop = false;
                 }
                 if(i < m - 1 && dp[i + 1][j - 1] > 0 && grid[i][j] > grid[i + 1][j - 1]) {
                     dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 1);
-                    stop = false;
                 }
-
                 ret = max(ret, dp[i][j] - 1);
-            }
-
-            if(stop) {
-                break;
             }
         }
 
