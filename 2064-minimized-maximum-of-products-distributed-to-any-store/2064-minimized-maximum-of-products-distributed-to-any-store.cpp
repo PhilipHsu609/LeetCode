49f3ddfc688e0792a451854a79d36dc792816d01
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2024-11-14 15:05:54 +0800

    Time: 61 ms (40.45%), Space: 88.4 MB (20.27%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        sort(begin(quantities), end(quantities));
+
+        int l = 1, r = quantities.back();
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+
+            if(canDistribute(quantities, mid, n)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+
+    bool canDistribute(const vector<int> &q, int k, int n) {
+        int m = q.size();
+        for(int i = 0; i < m; i++) {
+            if(q[i] <= k) {
+                n--;
+            } else {
+                n -= (q[i] + k - 1) / k;
+            }
+        }
+        return n >= 0;
+    }
+};
