13fa861a80cb6fe8c1025c3ecef4eb781965c76b
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-05-13 21:44:40 +0800

    Time: 883 ms (19.21%), Space: 495.1 MB (8.87%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        int n = s.length();
         int MOD = 1e9 + 7;
         vector<int> freq(26);
 
-        for(char ch : s) {
-            freq[ch - 'a']++;
+        for(char c : s) {
+            freq[c - 'a']++;
         }
 
         for(int i = 0; i < t; i++) {
             vector<int> nfreq(26);
-            for(int j = 0; j < 25; j++) {
-                nfreq[j + 1] = freq[j];
-            }
+            
+            nfreq[0] = nfreq[1] = freq[25];
 
-            nfreq[0] = freq[25];
-            nfreq[1] = (nfreq[1] + freq[25]) % MOD;
+            for(int i = 1; i < 26; i++) {
+                nfreq[i] = (nfreq[i] + freq[i - 1]) % MOD;
+            }
 
             freq = nfreq;
         }
 
         int ret = 0;
-        for(int v : freq) {
-            ret = (ret + v) % MOD;
+        for(int f : freq) {
+            ret = (ret + f) % MOD;
         }
 
         return ret;
