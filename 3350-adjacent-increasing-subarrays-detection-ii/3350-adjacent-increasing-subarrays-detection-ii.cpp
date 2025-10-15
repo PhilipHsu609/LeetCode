e8995cf949270f193cacef1af27d90ad0050e05e
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-10-15 22:08:17 +0800

    Time: 242 ms (27.44%), Space: 173.7 MB (81.58%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
-        nums.push_back(INT_MIN);
-        
         int n = nums.size();
-        int ret = 1;
-        int last_end = -1;
+        int ret = 0;
+        int len = 1;
         int last_len = 0;
 
-        for(int start = 0, end = 0; end < n; end++) {
-            if(end > 0 && nums[end - 1] >= nums[end]) {
-                if(last_end + 1 == start) {
-                    ret = max(ret, min(end - start, last_len));
-                }
-                last_end = end - 1;
-                last_len = end - start;
-                start = end;
+        for(int i = 1; i < n; i++) {
+            if(nums[i - 1] >= nums[i]) {
+                last_len = len;
+                len = 1;
+            } else {
+                len++;
             }
-            if((end - start + 1) % 2 == 0) {
-                ret = max(ret, (end - start + 1) / 2);
-            }
+            ret = max({ret, len / 2, min(len, last_len)});
         }
 
         return ret;
