01e3f88d9f9723d136f343a270a7333886caa804
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-11-02 11:20:42 +0800

    Time: 152 ms (34.86%), Space: 224.1 MB (11.47%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-    vector<int> dirs{0, 1, 0, -1, 0};
-
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> grid(m, vector<int>(n));
+        int ret = m * n - guards.size() - walls.size();
+
+        vector<vector<int>> board(m, vector<int>(n));
+        queue<array<int, 4>> q;
 
         for(const auto &g : guards) {
-            grid[g[0]][g[1]] = 2;
+            board[g[0]][g[1]] = 1;
+            q.push({g[0], g[1], 0, 1});
+            q.push({g[0], g[1], 1, 0});
+            q.push({g[0], g[1], -1, 0});
+            q.push({g[0], g[1], 0, -1});
         }
 
         for(const auto &w : walls) {
-            grid[w[0]][w[1]] = 3;
+            board[w[0]][w[1]] = 2;
         }
 
-        for(const auto &g : guards) {
-            dfs(grid, g[0], g[1]);
-        }
+        while(!q.empty()) {
+            auto [y, x, dy, dx] = q.front();
+            q.pop();
 
-        int ret = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 0) {
-                    ret++;
-                }
+            int xx = x + dx, yy = y + dy;
+            if(xx < 0 || yy < 0 || xx == n || yy == m) {
+                continue;
             }
+
+            if(board[yy][xx] == 1 || board[yy][xx] == 2) {
+                continue;
+            }
+
+            if(board[yy][xx] == 0) {
+                --ret;
+                board[yy][xx] = 3;
+            }
+
+            q.push({yy, xx, dy, dx});
         }
 
         return ret;
-    }
-
-    void dfs(vector<vector<int>> &grid, int i, int j) {
-        int m = grid.size(), n = grid[0].size();
-
-        for(int d = 0; d < 4; d++) {
-            int ii = i + dirs[d], jj = j + dirs[d + 1];
-
-            while(ii >= 0 && ii < m && jj >= 0 && jj < n && grid[ii][jj] < 2) {
-                grid[ii][jj] = 1;
-                ii += dirs[d];
-                jj += dirs[d + 1];
-            }
-        }
     }
 };
