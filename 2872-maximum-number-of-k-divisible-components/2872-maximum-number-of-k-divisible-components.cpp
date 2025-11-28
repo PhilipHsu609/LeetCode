9a7696a940416e6ad22b91802bbbdc6694ae55ed
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-11-28 22:41:03 +0800

    Time: 230 ms (11.03%), Space: 179.3 MB (47.33%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        unordered_map<int, vector<int>> adj;
+        vector<vector<int>> adj(n);
 
-        for(auto &e : edges) {
+        for(const auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
-        int ans = 0;
-        auto dfs = [&](auto dfs, int x, int p) -> int {
-            int val = values[x];
-            for(auto u : adj[x]) {
-                if(u != p) {
-                    val = (val + dfs(dfs, u, x)) % k;
+        function<int(int, int, int&)> dfs = [&](int cur, int prev, int &com) {
+            int sum = values[cur];
+            for(int v : adj[cur]) {
+                if(v == prev) {
+                    continue;
+                }
+                int vsum = dfs(v, cur, com);
+
+                if(vsum % k == 0) {
+                    com++;
+                } else {
+                    sum = (sum + vsum) % k;
                 }
             }
-            if(val % k == 0) {
-                ans++;
-                val = 0;
-            }
-
-            return val;
+            return sum;
         };
 
-        dfs(dfs, 0, -1);
-        return ans;
+        int ret = 0;
+        int sum = dfs(0, -1, ret);
+        ret += static_cast<int>(sum % k == 0);
+
+        return ret;
     }
 };
+
+/*
+
+def dfs(node, numComponents)
+    sum of left = visit left
+    sum of right = visit right
+
+    eval node {
+        if sum of left % k == 0
+            We found a new components
+        else
+            node value += sum of left
+        if sum of right % k == 0
+            We found a new components
+        else
+            node value += sum of right
+    }
+
+    return noe value
+*/
