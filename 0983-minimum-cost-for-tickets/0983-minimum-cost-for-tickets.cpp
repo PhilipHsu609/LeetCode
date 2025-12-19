42cc7a933bec1ae39a1875b0ade240524fab92d5
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-12-19 23:55:22 +0800

    Time: 0 ms (100%), Space: 12.7 MB (92.33%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int lastday = days.back();
-        vector<int> dp(lastday + 1, 0);
+        int n = days.back();
+        vector<int> dp(n + 1, INT_MAX);
 
-        for(int i = 0, day = 1; day <= lastday; day++) {
-            if(day < days[i]) {
-                dp[day] = dp[day - 1];
-            } else {
-                i++;
-                dp[day] = min({
-                    dp[day - 1] + costs[0],
-                    dp[max(0, day - 7)] + costs[1],
-                    dp[max(0, day - 30)] + costs[2]
-                });
+        dp[0] = 0;
+        for(int i = 1, d = 0; i <= n; ++i) {
+            if(i != days[d]) {
+                dp[i] = dp[i - 1];
+                continue;
             }
+            dp[i] = min(dp[i], dp[i - 1] + costs[0]);
+            dp[i] = min(dp[i], dp[max(0, i - 7)] + costs[1]);
+            dp[i] = min(dp[i], dp[max(0, i - 30)] + costs[2]);
+            ++d;
         }
 
-        return dp.back();
+        return dp[n];
     }
 };
