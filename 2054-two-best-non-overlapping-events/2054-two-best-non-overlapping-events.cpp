95363acf925c26b1de2c5636046e03326f1e7ead
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-12-23 22:46:21 +0800

    Time: 123 ms (46.33%), Space: 182.8 MB (33.59%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -4,19 +4,24 @@ public:
         vector<array<int, 3>> times;
 
         for(const auto &e : events) {
-            times.push_back({e[0], 1, e[2]});
-            times.push_back({e[1] + 1, 0, e[2]});
+            times.push_back({e[1] + 1, 0, e[2]}); // end time first
+            times.push_back({e[0], 1, e[2]}); // start time second
         }
 
-        sort(begin(times), end(times));
+        sort(times.begin(), times.end());
 
         int ret = 0;
-        int maxi= 0 ;
-        for(auto [t, start, v] : times) {
-            if(start == 1) {
-                ret = max(ret, maxi + v);
+        int maxv = 0;
+        for(auto [t, s, v] : times) {
+            if(s == 1) {
+                // start time
+                // try to pick this event
+                ret = max(ret, maxv + v);
             } else {
-                maxi = max(maxi, v);
+                // end time
+                // we reach the end of an event
+                // which ensures that the rest of the starting events wont overlap with the previous events
+                maxv = max(maxv, v);
             }
         }
 
