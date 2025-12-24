bac38b12e6ce397e74ef9b9b8c67e8a409926773
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-12-24 18:43:30 +0800

    Time: 8 ms (5.92%), Space: 40.2 MB (57.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,16 +4,14 @@ public:
         int n = nums.size();
         vector<int> ret(n, 1);
 
-        int prod = nums[0];
-        for(int i = 1; i < n; i++) {
-            ret[i] = prod;
-            prod *= nums[i];
+        for(int i = n - 2; i >= 0; --i) {
+            ret[i] = ret[i + 1] * nums[i + 1];
         }
 
-        prod = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            ret[i] *= prod;
-            prod *= nums[i];
+        int mul = 1;
+        for(int i = 0; i < n; ++i) {
+            ret[i] *= mul;
+            mul *= nums[i];
         }
 
         return ret;
