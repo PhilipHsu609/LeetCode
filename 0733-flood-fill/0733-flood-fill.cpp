0518dc56533344b898c796856ff2eb2b902d98bd
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2025-12-28 22:42:08 +0800

    Time: 0 ms (100%), Space: 18.2 MB (55.31%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size(), n = image[0].size();
         int oc = image[sr][sc];
 
         if(oc == color) {
             return image;
         }
 
-        queue<array<int, 2>> q;
+        int m = image.size(), n = image[0].size();
         vector<int> dir{0, 1, 0, -1, 0};
+        queue<array<int, 2>> q;
         q.push({sr, sc});
 
         while(!q.empty()) {
-            auto [y, x] = q.front();
+            auto [r, c] = q.front();
             q.pop();
 
-            image[y][x] = color;
+            if(r < 0 || c < 0 || r == m || c == n || image[r][c] != oc) {
+                continue;
+            }
+
+            image[r][c] = color;
+            
             for(int d = 0; d < 4; ++d) {
-                int ny = y + dir[d];
-                int nx = x + dir[d + 1];
-
-                if(ny < 0 || nx < 0 || ny == m || nx == n || oc != image[ny][nx]) {
-                    continue;
-                }
-
-                q.push({ny, nx});
+                int nr = r + dir[d];
+                int nc = c + dir[d + 1];
+                q.push({nr, nc});
             }
         }
 
