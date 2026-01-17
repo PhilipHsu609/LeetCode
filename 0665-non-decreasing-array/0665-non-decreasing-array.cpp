041d12f41a01ad3299dd0a90a290d5003ff7b885
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-01-17 19:55:42 +0800

    Time: 0 ms (100%), Space: 30.7 MB (71.97%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -3,14 +3,14 @@ public:
     bool checkPossibility(vector<int>& nums) {
         int cnt = 0;
 
-        for(int i = 0; i < nums.size() - 1; ++i) {
-            if(nums[i] > nums[i + 1]) {
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i - 1] > nums[i]) {
                 if(cnt++ == 1) {
                     return false;
                 }
 
-                if(i > 0 && nums[i - 1] > nums[i + 1]) {
-                    nums[i + 1] = nums[i];
+                if(i > 1 && nums[i - 2] > nums[i]) {
+                    nums[i] = nums[i - 1];
                 }
             }
         }
