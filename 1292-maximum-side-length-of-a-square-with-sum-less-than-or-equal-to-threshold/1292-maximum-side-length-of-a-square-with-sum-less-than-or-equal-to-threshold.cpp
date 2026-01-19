298acc7ea58988689f9c9365bce9d946fd65c289
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-01-19 09:08:17 +0800

    Time: 133 ms (16.36%), Space: 32.7 MB (60.91%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
         int m = mat.size(), n = mat[0].size();
-
         vector<vector<int>> prefix(m + 1, vector<int>(n + 1));
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] + mat[i - 1][j - 1] - prefix[i - 1][j - 1];
+
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                prefix[i][j] += prefix[i][j - 1] + mat[i - 1][j - 1];
             }
         }
-
-        int l = 0, r = min(m, n);
-        while(l < r) {
-            int mid = l + (r - l + 1) / 2;
-
-            bool ok = false;
-            for(int i = mid; i <= m && !ok; i++) {
-                for(int j = mid; j <= n; j++) {
-                    int sum = prefix[i][j] - prefix[i - mid][j] - prefix[i][j - mid] + prefix[i - mid][j - mid];
-                    if(sum <= threshold) {
-                        ok = true;
-                        break;
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                prefix[i][j] += prefix[i - 1][j];
+            }
+        }
+        
+        int maxside = 0;
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                for(int side = maxside + 1; side <= min(i, j); ++side) {
+                    int area = prefix[i][j] - prefix[i - side][j] - prefix[i][j - side] + prefix[i - side][j - side];
+                    if(area <= threshold) {
+                        maxside = max(maxside, side);
                     }
                 }
             }
-
-            if(ok) {
-                l = mid;
-            } else {
-                r = mid - 1;
-            }
         }
 
-        return l;
+        return maxside;
     }
 };
