9be66ffef7373d9a4c1002ca0b4831d40ca2adc3
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-01-19 09:21:13 +0800

    Time: 5 ms (89.09%), Space: 32.8 MB (36.82%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -3,17 +3,12 @@ public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
         int m = mat.size(), n = mat[0].size();
         vector<vector<int>> prefix(m + 1, vector<int>(n + 1));
-
-        for(int i = 1; i <= m; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + mat[i - 1][j - 1];
-            }
-        }
         
         int ret = 0;
         int side = 1;
         for(int i = 1; i <= m; ++i) {
             for(int j = 1; j <= n; ++j) {
+                prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + mat[i - 1][j - 1];
                 if(i < side || j < side) {
                     continue;
                 }
