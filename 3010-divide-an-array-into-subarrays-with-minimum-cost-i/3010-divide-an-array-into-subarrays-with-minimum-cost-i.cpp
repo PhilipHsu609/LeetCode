1a8a1de1c47bbba09f06948efe49eff1d5a7d4fb
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-02-01 14:28:11 +0800

    Time: 0 ms (100%), Space: 30.6 MB (48.24%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -6,18 +6,12 @@ public:
         int n = nums.size();
         int mini1 = INT_MAX, mini2 = INT_MAX;
         for(int i = 1; i < n; ++i) {
-            if(mini1 == INT_MAX) {
+            if(nums[i] < mini1) {
+                mini2 = min(mini2, mini1);
                 mini1 = nums[i];
-            } else if(mini2 == INT_MAX) {
+            } else if(nums[i] < mini2) {
+                mini1 = min(mini1, mini2);
                 mini2 = nums[i];
-            } else {
-                if(nums[i] < mini1) {
-                    mini2 = min(mini2, mini1);
-                    mini1 = nums[i];
-                } else if(nums[i] < mini2) {
-                    mini1 = min(mini1, mini2);
-                    mini2 = nums[i];
-                }
             }
         }
         return ret + mini1 + mini2;
