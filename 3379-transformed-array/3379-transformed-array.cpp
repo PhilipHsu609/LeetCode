e4c5a314e2e4bab3df43b117e9ae991a5295653e
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-02-05 20:16:59 +0800

    Time: 0 ms (100%), Space: 25.7 MB (39.55%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ret(nums);
+
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] > 0) {
+                ret[i] = nums[(i + nums[i]) % n];
+            } else if(nums[i] < 0) {
+                ret[i] = nums[((i + nums[i]) % n + n) % n];
+            }
+        }
+
+        return ret;
+    }
+};
