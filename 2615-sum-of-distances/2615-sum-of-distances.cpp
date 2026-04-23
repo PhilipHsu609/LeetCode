e542ca7d299df25bc22ee54fc4ee1f50d573d3ec
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-04-23 19:27:52 +0800

    Time: 119 ms (16.5%), Space: 127.8 MB (22.75%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     vector<long long> distance(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, vector<int>> idx;
+        unordered_map<int, int> cnt;
+        unordered_map<int, vector<long long>> prefix;
+        vector<int> idx(n);
         vector<long long> ret(n);
 
         for(int i = 0; i < n; ++i) {
-            idx[nums[i]].push_back(i);
-        }
-
-        unordered_map<int, vector<long long>> prefix;
-        for(const auto &[k, v] : idx) {
-            prefix[k].resize(v.size() + 1);
-            for(int i = 1; i <= v.size(); ++i) {
-                prefix[k][i] = prefix[k][i - 1] + idx[k][i - 1];
+            idx[i] = ++cnt[nums[i]];
+            if(prefix[nums[i]].empty()) {
+                prefix[nums[i]].push_back(0);
             }
+            prefix[nums[i]].push_back(prefix[nums[i]].back() + i);
         }
 
         for(int i = 0; i < n; ++i) {
-            auto it = lower_bound(idx[nums[i]].begin(), idx[nums[i]].end(), i);
-            int l = std::distance(idx[nums[i]].begin(), it);
+            int l = idx[i] - 1;
             if(l > 0) {
                 ret[i] = 1LL * i * l - prefix[nums[i]][l];
             }
-            int r = idx[nums[i]].size() - l - 1;
+            int r = cnt[nums[i]] - l - 1;
             if(r > 0) {
                 ret[i] += (prefix[nums[i]].back() - prefix[nums[i]][l + 1]) - 1LL * i * r;
             }
