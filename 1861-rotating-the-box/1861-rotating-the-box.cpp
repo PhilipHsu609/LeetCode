dbb2a4eb9a88f33e06e738e3e03a1b4e19d05c2b
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-05-06 19:08:50 +0800

    Time: 54 ms (11.3%), Space: 72.3 MB (5.41%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int m = box.size(), n = box[0].size();
-        vector<vector<char>> ret(n, vector<char>(m, '.'));
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size(), n = boxGrid[0].size();
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                ret[j][m - i - 1] = box[i][j];
+        vector<int> cnt(m);
+        unordered_map<int, int> pat;
+
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(boxGrid[i][j] == '#') {
+                    ++cnt[i];
+                }
+
+                if(boxGrid[i][j] == '*') {
+                    pat[i * n + j] = cnt[i];
+                    cnt[i] = 0;
+                }
             }
         }
 
-        for(int j = 0; j < m; j++) {
-            int k = n - 1;
-            for(int i = n - 1; i >= 0; i--) {
-                if(ret[i][j] == '*') {
-                    k = i - 1;
+        vector<vector<char>> ret(n, vector<char>(m, '.'));
+
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                if(boxGrid[m - j - 1][i] == '*') {
+                    ret[i][j] = '*';
+                    cnt[m - j - 1] = pat[(m - j - 1) * n + i];
+                    continue;
                 }
-                if(ret[i][j] == '#') {
-                    ret[i][j] = '.';
-                    ret[k][j] = '#';
-                    k--;
+
+                if(cnt[m - j - 1] > 0) {
+                    ret[i][j] = '#';
+                    --cnt[m - j - 1];
                 }
             }
         }
