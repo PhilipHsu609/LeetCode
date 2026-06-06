4be9a5ee3e52c17181859abfd2f50da3b5bad7c0
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-06-06 17:05:31 +0800

    Time: 0 ms (100%), Space: 13.9 MB (32.98%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -6,28 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
         queue<TreeNode*> q;
+
         q.push(root);
 
-        bool nomore = false;
-        while(!q.empty()) {
-            auto *u = q.front();
-            q.pop();
+        bool hitNull = false;
+        while (!q.empty()) {
+            for (int s = q.size(); s > 0; --s) {
+                auto* node = q.front();
+                q.pop();
 
-            if(u == nullptr) {
-                nomore = true;
-            } else {
-                if(nomore) {
+                if(hitNull && node != nullptr) {
                     return false;
                 }
-                q.push(u->left);
-                q.push(u->right);
+
+                if (node != nullptr) {
+                    q.push(node->left);
+                    q.push(node->right);
+                } else {
+                    hitNull = true;
+                }
             }
         }
 
